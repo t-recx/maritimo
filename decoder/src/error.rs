4874d32ee653407c80792f88b0b65e94df417553
João Bruno <hi@joaobruno.xyz>
2022-05-13 23:20:58 +0100

    Added decoder parameterization

--- decoder/src/error.rs
+++ decoder/src/error.rs
@@ -1,3 +1,6 @@
+use std::error::Error;
+use std::fmt;
+
 #[derive(Debug, Clone, PartialEq)]
 pub enum NMEADecoderErrorType {
     IncorrectPartNumber,
@@ -13,4 +16,21 @@ pub enum NMEADecoderErrorType {
 #[derive(Debug, Clone)]
 pub struct NMEADecoderError {
     pub error_type: NMEADecoderErrorType,
+}
+
+#[derive(Debug, Clone)]
+pub struct MissingEnvironmentVariableError {
+    pub message: String,
+}
+
+impl fmt::Display for MissingEnvironmentVariableError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
+
+impl Error for MissingEnvironmentVariableError {
+    fn description(&self) -> &str {
+        &self.message
+    }
 }
