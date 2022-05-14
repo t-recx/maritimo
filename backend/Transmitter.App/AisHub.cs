9501cd8ca6c7d5379ba6fc382bd9d72711d02fb8
João Bruno <hi@joaobruno.xyz>
2022-05-15 00:59:17 +0100

    Moved interfaces to own file

--- backend/Transmitter.App/AisHub.cs
+++ backend/Transmitter.App/AisHub.cs
@@ -2,11 +2,6 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace Transmitter.App;
 
-public interface IAisHub
-{
-    Task Receive(DTOObjectData objectData);
-}
-
 public class AisHub : Hub<IAisHub>
 {
 }
