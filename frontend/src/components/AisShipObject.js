b69524f85273f06a83c33a666d8a39ac6684164a
João Bruno <hi@joaobruno.xyz>
2022-05-28 19:37:39 +0100

    Now changing ship color depending on ship type

--- frontend/src/components/AisShipObject.js
+++ frontend/src/components/AisShipObject.js
@@ -2,49 +2,86 @@ import * as turf from "@turf/turf";
 import React, { useState, useEffect } from "react";
 import { Polygon, Circle, Marker } from "react-leaflet";
 import L from "leaflet";
+import { getShipColorScheme } from "../shipColorSchemes";
 
-// todo: change ship color according to type
 // todo: change ship svg when ship is stopped
 // todo: change (and only use) svg when it's not a ship (example: weather station/ais station)
+
+const DisplayTypes = {
+  ShipPolygon: 1,
+  Circle: 2,
+};
+
 function AisShipObject({ data, zoom }) {
-  const color = "darkblue";
-  const fillColor = "blue";
-  const pathOptions = { color, fillColor };
-  const circlePathOptions = { fillColor, color };
+  const [displayType, setDisplayType] = useState(null);
   const [objectPolygon, setObjectPolygon] = useState(null);
   const [iconShip, setIconShip] = useState(null);
   const [iconLocation, setIconLocation] = useState([]);
+  const [colorScheme, setColorScheme] = useState(null);
+  const [pathOptions, setPathOptions] = useState(null);
+  const [transmitterPathOptions, setTransmitterPathOptions] = useState(null);
 
   useEffect(() => {
-    if (data.true_heading) {
+    if (data) {
+      if (
+        data.dimension_to_bow &&
+        data.dimension_to_port &&
+        data.dimension_to_starboard &&
+        data.dimension_to_stern &&
+        data.true_heading
+      ) {
+        setDisplayType(DisplayTypes.ShipPolygon);
+      } else {
+        setDisplayType(DisplayTypes.Circle);
+      }
+    } else {
+      setDisplayType(null);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    const colorScheme = getShipColorScheme(data.ship_type);
+
+    setColorScheme(colorScheme);
+    setPathOptions({
+      color: colorScheme.color,
+      fillColor: colorScheme.fillColor,
+    });
+    setTransmitterPathOptions({
+      color: colorScheme.colorTransmitter,
+      fillColor: colorScheme.fillColorTransmitter,
+    });
+  }, [data.ship_type]);
+
+  useEffect(() => {
+    if (data.true_heading && colorScheme) {
       const angle = data.true_heading;
       const i = new L.DivIcon({
         html:
-          '<svg xmlns="http://www.w3.org/2000/svg"  width="32" height="32"" transform="translate(-10,-10) scale(0.5)" > \
-            <polygon stroke="' +
-          color +
+          '<svg xmlns="http://www.w3.org/2000/svg"  width="32" height="32"" transform="translate(-10,-10) scale(0.5)" > <polygon stroke="' +
+          colorScheme.color +
           '" stroke-width=1  fill="' +
-          fillColor +
+          colorScheme.fillColor +
           '" transform="rotate(' +
           angle +
-          ' 16, 16)" points=" 12,14 12,28 16,24 20,28, 20,14 16,4" /> \
-            </svg>',
+          ' 16, 16)" points=" 12,14 12,28 16,24 20,28, 20,14 16,4" /> </svg>',
         className: "",
       });
       setIconShip(i);
     } else {
       setIconShip(null);
     }
-  }, [data.true_heading]);
+  }, [data.true_heading, colorScheme]);
 
   useEffect(() => {
-    if (
-      data.dimension_to_bow &&
-      data.dimension_to_port &&
-      data.dimension_to_starboard &&
-      data.dimension_to_stern &&
-      data.true_heading
-    ) {
+    if (displayType === DisplayTypes.ShipPolygon) {
+      //if (
+      //data.dimension_to_bow &&
+      //data.dimension_to_port &&
+      //data.dimension_to_starboard &&
+      //data.dimension_to_stern &&
+      //data.true_heading
+      //) {
       const angle = ((data.true_heading + 180) % 360) - 180;
 
       const beakSize = (data.dimension_to_bow + data.dimension_to_stern) / 10;
@@ -98,7 +135,7 @@ function AisShipObject({ data, zoom }) {
     } else {
       setObjectPolygon(null);
     }
-  }, [data]);
+  }, [data, displayType]);
 
   useEffect(() => {
     if (objectPolygon) {
@@ -119,23 +156,34 @@ function AisShipObject({ data, zoom }) {
 
   return (
     <React.Fragment>
-      {zoom >= 14 && objectPolygon && (
+      {displayType === DisplayTypes.ShipPolygon && (
         <React.Fragment>
-          <Polygon
-            pathOptions={pathOptions}
-            positions={objectPolygon}
-          ></Polygon>
+          {zoom >= 14 && objectPolygon && (
+            <React.Fragment>
+              <Polygon
+                pathOptions={pathOptions}
+                positions={objectPolygon}
+              ></Polygon>
+              {zoom >= 17 && (
+                <Circle
+                  center={[data.latitude, data.longitude]}
+                  pathOptions={transmitterPathOptions}
+                  radius={0.1}
+                ></Circle>
+              )}
+            </React.Fragment>
+          )}
+          {zoom < 14 && objectPolygon && iconShip && (
+            <Marker position={iconLocation} icon={iconShip}></Marker>
+          )}
         </React.Fragment>
       )}
-      {(zoom >= 17 || !objectPolygon) && (
+      {displayType === DisplayTypes.Circle && pathOptions && (
         <Circle
           center={[data.latitude, data.longitude]}
-          pathOptions={circlePathOptions}
-          radius={0.1}
+          pathOptions={pathOptions}
+          radius={1}
         ></Circle>
-      )}
-      {zoom < 14 && objectPolygon && iconShip && (
-        <Marker position={iconLocation} icon={iconShip}></Marker>
       )}
     </React.Fragment>
   );
---
+++ frontend/src/shipTypes.js
@@ -0,0 +1,19 @@
+const ShipTypes = {
+  CargoShip: [70, 71, 72, 73, 74, 75, 76, 77, 78, 79],
+  TankerShip: [80, 81, 82, 83, 84, 85, 86, 87, 88, 89],
+  PassengerShip: [60, 61, 62, 63, 64, 65, 66, 67, 68, 69],
+  HighSpeedCraftShip: [40, 41, 42, 43, 44, 45, 46, 47, 48, 49],
+  FishingShip: [30],
+  TowingShip: [31, 32, 52],
+  MilitaryShip: [35],
+  SailingShip: [36],
+  LawEnforcementShip: [55],
+  PleasureCraft: [37],
+  PilotVessel: [50],
+  SearchAndRescueVessel: [51],
+  AntiPolutionEquipment: [54],
+  MedicalTransport: [58],
+  WingInGround: [20, 21, 22, 23, 24, 25, 26, 27, 28, 29],
+};
+
+export default ShipTypes;
