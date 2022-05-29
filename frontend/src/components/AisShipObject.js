31e21887b0695b9e0ed0dc15ee07b6bf469a7c68
João Bruno <hi@joaobruno.xyz>
2022-05-29 21:49:31 +0100

    Now using an svg for ships that have no dimensions/heading

--- frontend/src/components/AisShipObject.js
+++ frontend/src/components/AisShipObject.js
@@ -7,31 +7,13 @@ import { getShipColorScheme } from "../shipColorSchemes";
 // todo: change ship svg when ship is stopped
 // todo: change (and only use) svg when it's not a ship (example: weather station/ais station)
 
-const DisplayTypes = {
-  ShipPolygon: 1,
-  Circle: 2,
-};
-
 function AisShipObject({ data, zoom }) {
-  const [displayType, setDisplayType] = useState(null);
   const [objectPolygon, setObjectPolygon] = useState(null);
-  const [iconShip, setIconShip] = useState(null);
+  const [icon, setIcon] = useState(null);
   const [iconLocation, setIconLocation] = useState([]);
   const [colorScheme, setColorScheme] = useState(null);
   const [pathOptions, setPathOptions] = useState(null);
   const [transmitterPathOptions, setTransmitterPathOptions] = useState(null);
-
-  useEffect(() => {
-    if (data) {
-      if (canBePolygon(data)) {
-        setDisplayType(DisplayTypes.ShipPolygon);
-      } else {
-        setDisplayType(DisplayTypes.Circle);
-      }
-    } else {
-      setDisplayType(null);
-    }
-  }, [data]);
 
   useEffect(() => {
     const colorScheme = getShipColorScheme(data.ship_type);
@@ -48,27 +30,40 @@ function AisShipObject({ data, zoom }) {
   }, [data.ship_type]);
 
   useEffect(() => {
-    if (data.true_heading && colorScheme) {
-      const angle = data.true_heading;
-      const i = new L.DivIcon({
-        html:
-          '<svg xmlns="http://www.w3.org/2000/svg"  width="32" height="32"" transform="translate(-10,-10) scale(0.5)" > <polygon stroke="' +
-          colorScheme.color +
-          '" stroke-width=1  fill="' +
-          colorScheme.fillColor +
-          '" transform="rotate(' +
-          angle +
-          ' 16, 16)" points=" 12,14 12,28 16,24 20,28, 20,14 16,4" /> </svg>',
-        className: "",
-      });
-      setIconShip(i);
+    if (colorScheme) {
+      if (data.true_heading) {
+        const angle = data.true_heading;
+        const i = new L.DivIcon({
+          html:
+            '<svg xmlns="http://www.w3.org/2000/svg"  width="32" height="32" transform="translate(-10,-10) scale(0.5)" > <polygon stroke="' +
+            colorScheme.color +
+            '" stroke-width=1  fill="' +
+            colorScheme.fillColor +
+            '" transform="rotate(' +
+            angle +
+            ' 16, 16)" points=" 12,14 12,28 16,24 20,28, 20,14 16,4" /> </svg>',
+          className: "",
+        });
+        setIcon(i);
+      }
+      else {
+        const i = new L.DivIcon({
+          html:
+            '<svg xmlns="http://www.w3.org/2000/svg"  width="8" height="8" transform="translate(3, -3)"> <circle ' + 
+            '"  fill="' +
+            colorScheme.fillColor +
+            '" cx="4" cy="4" r="3" /> </svg>',
+          className: "",
+        });
+        setIcon(i);
+      }
     } else {
-      setIconShip(null);
+      setIcon(null);
     }
   }, [data.true_heading, colorScheme]);
 
   useEffect(() => {
-    if (displayType === DisplayTypes.ShipPolygon && canBePolygon(data)) {
+    if (canBePolygon(data)) {
       const angle = ((data.true_heading + 180) % 360) - 180;
 
       const beakSize = (data.dimension_to_bow + data.dimension_to_stern) / 10;
@@ -122,7 +117,7 @@ function AisShipObject({ data, zoom }) {
     } else {
       setObjectPolygon(null);
     }
-  }, [data, displayType]);
+  }, [data]);
 
   useEffect(() => {
     if (objectPolygon) {
@@ -154,34 +149,32 @@ function AisShipObject({ data, zoom }) {
 
   return (
     <React.Fragment>
-      {displayType === DisplayTypes.ShipPolygon && (
+      {zoom >= 14 && objectPolygon && (
         <React.Fragment>
-          {zoom >= 14 && objectPolygon && (
-            <React.Fragment>
-              <Polygon
-                pathOptions={pathOptions}
-                positions={objectPolygon}
-              ></Polygon>
-              {zoom >= 17 && (
-                <Circle
-                  center={[data.latitude, data.longitude]}
-                  pathOptions={transmitterPathOptions}
-                  radius={0.1}
-                ></Circle>
-              )}
-            </React.Fragment>
-          )}
-          {zoom < 14 && objectPolygon && iconShip && (
-            <Marker position={iconLocation} icon={iconShip}></Marker>
+          <Polygon
+            pathOptions={pathOptions}
+            positions={objectPolygon}
+          ></Polygon>
+          {zoom >= 17 && (
+            <Circle
+              center={[data.latitude, data.longitude]}
+              pathOptions={transmitterPathOptions}
+              radius={0.1}
+            ></Circle>
           )}
         </React.Fragment>
       )}
-      {displayType === DisplayTypes.Circle && pathOptions && (
-        <Circle
-          center={[data.latitude, data.longitude]}
-          pathOptions={pathOptions}
-          radius={1}
-        ></Circle>
+      {zoom >= 14 && !objectPolygon && (
+        <React.Fragment>
+            <Circle
+              center={[data.latitude, data.longitude]}
+              pathOptions={pathOptions}
+              radius={0.1}
+            ></Circle>
+        </React.Fragment>
+      )}
+      {zoom < 14 && icon && (
+        <Marker position={iconLocation} icon={icon}></Marker>
       )}
     </React.Fragment>
   );
