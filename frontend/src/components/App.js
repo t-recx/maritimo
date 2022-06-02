ca4d268fc6d91d4818f9af49ed9d7f37f727f536
João Bruno <hi@joaobruno.xyz>
2022-06-02 07:00:54 +0100

    Fixed truncated map

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -8,22 +8,24 @@ import React from "react";
 function App() {
   return (
     <React.Fragment>
-      <Navbar />
-      <MapContainer
-        center={[
-          parseFloat(process.env.REACT_APP_MAP_INITIAL_CENTER_LATITUDE),
-          parseFloat(process.env.REACT_APP_MAP_INITIAL_CENTER_LONGITUDE),
-        ]}
-        zoom={process.env.REACT_APP_MAP_INITIAL_ZOOM}
-        maxZoom={process.env.REACT_APP_MAP_MAX_ZOOM}
-      >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
+      <div className="main-container">
+        <Navbar />
+        <MapContainer
+          center={[
+            parseFloat(process.env.REACT_APP_MAP_INITIAL_CENTER_LATITUDE),
+            parseFloat(process.env.REACT_APP_MAP_INITIAL_CENTER_LONGITUDE),
+          ]}
+          zoom={process.env.REACT_APP_MAP_INITIAL_ZOOM}
+          maxZoom={process.env.REACT_APP_MAP_MAX_ZOOM}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
 
-        <AisMap />
-      </MapContainer>
+          <AisMap />
+        </MapContainer>
+      </div>
     </React.Fragment>
   );
 }
