9ba4f5211f388292f5c09f6078d51833b2f744ff
João Bruno <hi@joaobruno.xyz>
2022-07-23 22:49:23 +0100

    Added filters by country code, ship type and mmsi/name/imo

--- frontend/src/components/Vessels.js
+++ frontend/src/components/Vessels.js
@@ -3,26 +3,102 @@ import React, { useEffect, useState } from "react";
 import { useNavigate, useSearchParams, useLocation } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { FlagsByMid, getCountryDescription, getFlagInformation } from "../mmsi";
-import { getShipTypeDescription } from "../shipTypes";
+import { getShipTypeDescription, ShipTypes } from "../shipTypes";
 import Loading from "./Loading";
 import Pagination from "./Pagination";
 import "./Vessels.css";
 
 function Vessels() {
   const [search, setSearch] = useSearchParams();
+  const location = useLocation();
   const [pageNumber, setPageNumber] = useState(null);
   const [pageSize, setPageSize] = useState(null);
   const [totalPages, setTotalPages] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [vessels, setVessels] = useState(null);
-  const [searchCountryCode, setSearchCountryCode] = useState(null);
-  const [selectedCountryCode, setSelectedCountryCode] = useState(null);
-
-  const location = useLocation();
+  const [searchCountryCodes, setSearchCountryCodes] = useState("");
+  const [searchShipTypes, setSearchShipTypes] = useState("");
+  const [searchText, setSearchText] = useState("");
+  const [countryCodeFilterDataSource, setCountryCodeFilterDataSource] =
+    useState(null);
+  const [shipTypesDataSource, setShipTypesDataSource] = useState(null);
 
   const navigate = useNavigate();
 
   useEffect(() => {
+    let dataSource = null;
+
+    const flagsEntries = Object.entries(FlagsByMid);
+    const countries = [
+      ...new Set(Object.values(FlagsByMid).map((x) => x.country)),
+    ];
+
+    dataSource = countries
+      .map((country) => [
+        flagsEntries
+          .filter((x) => x[1].country == country)
+          .map((x) => x[0])
+          .join(","),
+        country,
+      ])
+      .sort((a, b) => a[1].localeCompare(b[1]));
+
+    setCountryCodeFilterDataSource(dataSource);
+  }, []);
+
+  useEffect(() => {
+    let dataSource = [];
+
+    dataSource.push([ShipTypes.CargoShip.join(","), "Cargo"]);
+    dataSource.push([ShipTypes.TankerShip.join(","), "Tanker"]);
+    dataSource.push([ShipTypes.PassengerShip.join(","), "Passenger"]);
+    dataSource.push([
+      ShipTypes.HighSpeedCraftShip.join(","),
+      "HighSpeed craft",
+    ]);
+    dataSource.push([ShipTypes.FishingShip.join(","), "Fishing"]);
+    dataSource.push([ShipTypes.TowingShip.join(","), "Towing"]);
+    dataSource.push([ShipTypes.MilitaryShip.join(","), "Military"]);
+    dataSource.push([ShipTypes.SailingShip.join(","), "Sailing"]);
+    dataSource.push([
+      ShipTypes.LawEnforcementShip.join(","),
+      "Law Enforcement",
+    ]);
+    dataSource.push([ShipTypes.PleasureCraft.join(","), "Pleasure craft"]);
+    dataSource.push([ShipTypes.PilotVessel.join(","), "Pilot vessel"]);
+    dataSource.push([
+      ShipTypes.SearchAndRescueVessel.join(","),
+      "Search and rescue",
+    ]);
+    dataSource.push([
+      ShipTypes.AntiPolutionEquipment.join(","),
+      "Anti-polution equipment",
+    ]);
+    dataSource.push([ShipTypes.MedicalTransport.join(","), "Medical"]);
+    dataSource.push([ShipTypes.WingInGround.join(","), "Wing in ground"]);
+
+    dataSource = dataSource.sort((a, b) => a[1].localeCompare(b[1]));
+
+    setShipTypesDataSource(dataSource);
+  }, []);
+
+  useEffect(() => {
+    let params = getFromSearch();
+
+    setPageNumber(params.paramPageNumber);
+    setPageSize(params.paramPageSize);
+
+    setSearchCountryCodes(params.paramCountryCodes || "");
+
+    setSearchShipTypes(params.paramShipTypes || "");
+    setSearchText(params.paramSearchText || "");
+  }, [search]);
+
+  useEffect(() => {
+    fetchData();
+  }, [search]);
+
+  function getFromSearch() {
     let paramPageNumber = parseInt(search.get("pageNumber"));
 
     if (
@@ -43,80 +119,115 @@ function Vessels() {
       paramPageSize = 10;
     }
 
-    setPageNumber(paramPageNumber);
-    setPageSize(paramPageSize);
+    let paramCountryCodes = search.get("countryCodes");
 
-    let paramCountryCode = parseInt(search.get("countryCode"));
-
-    if (paramCountryCode == null || Number.isNaN(paramCountryCode)) {
-      setSearchCountryCode(null);
-      setSelectedCountryCode(null);
-    } else {
-      setSearchCountryCode(paramCountryCode);
-      setSelectedCountryCode(paramCountryCode);
+    if (paramCountryCodes == null || paramCountryCodes.toString().length == 0) {
+      paramCountryCodes = null;
     }
-  }, [search]);
+    let paramShipTypes = search.get("shipTypes");
 
-  useEffect(() => {
-    const controller = new AbortController();
+    if (paramShipTypes == null || paramShipTypes.toString().length == 0) {
+      paramShipTypes = null;
+    }
+    let paramSearchText = search.get("text");
 
-    async function fetchData() {
-      try {
-        if (pageNumber != null && pageSize != null) {
-          setIsLoading(true);
+    if (paramSearchText == null || paramSearchText.length == 0) {
+      paramSearchText = null;
+    }
 
-          const result = await axios.get(
-            process.env.REACT_APP_WEB_API_URL + "/vessel",
-            {
-              withCredentials: true,
-              params: {
-                pageNumber: pageNumber,
-                pageSize: pageSize,
-                countryCode: searchCountryCode,
-              },
-              headers: {
-                "Content-Type": "application/json",
-              },
-            }
-          );
+    return {
+      paramPageNumber: paramPageNumber,
+      paramPageSize: paramPageSize,
+      paramSearchText: paramSearchText,
+      paramShipTypes: paramShipTypes,
+      paramCountryCodes: paramCountryCodes,
+    };
+  }
 
-          setVessels(result.data);
-          setTotalPages(result.data.totalPages);
-          if (result.data && result.data.items) {
-            result.data.items.forEach((dto) => {
-              dto.countryName = getCountryDescription(dto.mmsi);
-              dto.flagInformation = getFlagInformation(dto.mmsi);
-              dto.shipTypeDescription = getShipTypeDescription(dto.ship_type);
-            });
-          }
+  function getParams() {
+    let paramsFromSearch = getFromSearch();
 
-          setIsLoading(false);
+    return {
+      pageNumber: paramsFromSearch.paramPageNumber,
+      pageSize: paramsFromSearch.paramPageSize,
+      countryCodes: paramsFromSearch.paramCountryCodes,
+      shipTypes: paramsFromSearch.paramShipTypes,
+      text: paramsFromSearch.paramSearchText,
+    };
+  }
+
+  async function fetchData() {
+    try {
+      setIsLoading(true);
+
+      const result = await axios.get(
+        process.env.REACT_APP_WEB_API_URL + "/vessel",
+        {
+          withCredentials: true,
+          params: getParams(),
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
-        console.log("finished fetching data");
-      } catch (error) {
-        console.error(error);
+      );
+
+      setVessels(result.data);
+      setTotalPages(result.data.totalPages);
+      if (result.data && result.data.items) {
+        result.data.items.forEach((dto) => {
+          dto.countryName = getCountryDescription(dto.mmsi);
+          dto.flagInformation = getFlagInformation(dto.mmsi);
+          dto.shipTypeDescription = getShipTypeDescription(dto.ship_type);
+        });
       }
+
+      setIsLoading(false);
+    } catch (error) {
+      console.error(error);
     }
-
-    fetchData();
-
-    return () => controller.abort();
-  }, [pageNumber, pageSize, searchCountryCode]);
+  }
 
   function navigateToVessel(mmsi) {
-    navigate("/vessel/" + mmsi);
+    //navigate("/vessel/" + mmsi);
   }
 
   function handleCountryCodeChange(event) {
-    setSelectedCountryCode(event.target.value);
+    setSearchCountryCodes(event.target.value);
+  }
+
+  function handleTextSearchChange(event) {
+    setSearchText(event.target.value);
+  }
+
+  function handleKeyDownSearch(event) {
+    if (event.key === "Enter") {
+      searchData();
+    }
+  }
+
+  function handleShipTypeChange(event) {
+    setSearchShipTypes(event.target.value);
   }
 
   function searchData() {
     let updatedSearch = {};
-    setSearchCountryCode(selectedCountryCode);
 
-    if (selectedCountryCode) {
-      updatedSearch = { ...updatedSearch, countryCode: selectedCountryCode };
+    if (searchCountryCodes) {
+      updatedSearch = {
+        ...updatedSearch,
+        countryCodes: searchCountryCodes,
+      };
+    }
+
+    if (searchShipTypes) {
+      updatedSearch = {
+        ...updatedSearch,
+        shipTypes: searchShipTypes,
+      };
+    }
+
+    if (searchText && searchText.length > 0) {
+      updatedSearch = { ...updatedSearch, text: searchText };
     }
 
     if (search.get("pageNumber")) {
@@ -131,6 +242,12 @@ function Vessels() {
     }
 
     setSearch(updatedSearch, { replace: true });
+  }
+
+  function resetSearchFilters() {
+    setSearchCountryCodes("");
+    setSearchShipTypes("");
+    setSearchText("");
   }
 
   return (
@@ -148,13 +265,14 @@ function Vessels() {
                     <div className="control">
                       <div className="select is-fullwidth">
                         <select
-                          value={selectedCountryCode}
+                          value={searchCountryCodes}
                           onChange={handleCountryCodeChange}
+                          onKeyDown={handleKeyDownSearch}
                         >
-                          <option></option>
-                          {Object.keys(FlagsByMid).map((key) => (
-                            <option key={key} value={key}>
-                              {FlagsByMid[key].country}
+                          <option value=""></option>
+                          {countryCodeFilterDataSource.map((x) => (
+                            <option key={x[0]} value={x[0]}>
+                              {x[1]}
                             </option>
                           ))}
                         </select>
@@ -168,8 +286,17 @@ function Vessels() {
                     <label className="label">Type</label>
                     <div className="control">
                       <div className="select is-fullwidth">
-                        <select>
-                          <option></option>
+                        <select
+                          value={searchShipTypes}
+                          onChange={handleShipTypeChange}
+                          onKeyDown={handleKeyDownSearch}
+                        >
+                          <option value=""></option>
+                          {shipTypesDataSource.map((x) => (
+                            <option key={x[0]} value={x[0]}>
+                              {x[1]}
+                            </option>
+                          ))}
                         </select>
                       </div>
                     </div>
@@ -180,19 +307,31 @@ function Vessels() {
                   <div className="field">
                     <label className="label">MMSI/IMO/Name</label>
                     <div className="control">
-                      <input className="input" type="text" placeholder="" />
+                      <input
+                        className="input"
+                        type="text"
+                        placeholder=""
+                        value={searchText}
+                        onChange={handleTextSearchChange}
+                        onKeyDown={handleKeyDownSearch}
+                      />
                     </div>
                   </div>
                 </div>
               </div>
-              <div class="field is-grouped is-justify-content-end">
-                <div class="control">
-                  <button class="button is-link" onClick={searchData}>
+              <div className="field is-grouped is-justify-content-end">
+                <div className="control">
+                  <button className="button is-link" onClick={searchData}>
                     Search
                   </button>
                 </div>
-                <div class="control">
-                  <button class="button is-link is-light">Reset</button>
+                <div className="control">
+                  <button
+                    className="button is-link is-light"
+                    onClick={resetSearchFilters}
+                  >
+                    Reset
+                  </button>
                 </div>
               </div>
             </div>
@@ -208,8 +347,8 @@ function Vessels() {
               <tbody>
                 {vessels.items.map((item) => (
                   <tr
-                    className="is-clickable"
-                    key={item.stationId}
+                    className=""
+                    key={item.mmsi}
                     onClick={() => navigateToVessel(item.mmsi)}
                   >
                     <td className="td-country is-hidden-mobile">
@@ -247,9 +386,7 @@ function Vessels() {
                             e.stopPropagation();
                           }}
                         >
-                          <Link to={"/vessel/" + item.mmsi}>
-                            {item.name || "Unknown"}
-                          </Link>
+                          {item.name || "Unknown"}
                         </span>
                       </div>
                     </td>
