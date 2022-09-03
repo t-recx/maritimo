d1198a1185880f503108724b01bae77509acc12a
João Bruno <jnnlbruno@gmail.com>
2022-09-03 01:01:25 +0100

    Trimmed dto to discard fields that are not used in the frontend

--- backend/WebApi.App/DTOWebObjectData.cs
+++ backend/WebApi.App/DTOWebObjectData.cs
@@ -7,29 +7,16 @@ public class DTOWebObjectData
     public byte? navigation_status { get; set; }
     public float rate_of_turn { get; set; }
     public float? speed_over_ground { get; set; }
-    public bool? position_accuracy { get; set; }
     public float? longitude { get; set; }
     public float? latitude { get; set; }
     public float? course_over_ground { get; set; }
     public int? true_heading { get; set; }
-    public byte? timestamp { get; set; }
-    public byte? manuever_indicator { get; set; }
-    public bool? raim_flag { get; set; }
-    public bool? cs_unit { get; set; }
-    public bool? display_flag { get; set; }
-    public bool? dsc_flag { get; set; }
-    public bool? band_flag { get; set; }
-    public bool? message_22_flag { get; set; }
-    public bool? assigned { get; set; }
     public string? name { get; set; }
     public byte? ship_type { get; set; }
     public ushort? dimension_to_bow { get; set; }
     public ushort? dimension_to_stern { get; set; }
     public byte? dimension_to_port { get; set; }
     public byte? dimension_to_starboard { get; set; }
-    public byte? position_fix_type { get; set; }
-    public bool? dte { get; set; }
-    public byte? ais_version { get; set; }
     public uint? imo_number { get; set; }
     public string? call_sign { get; set; }
     public byte? eta_month { get; set; }
@@ -38,7 +25,6 @@ public class DTOWebObjectData
     public byte? eta_minute { get; set; }
     public float? draught { get; set; }
     public string? destination { get; set; }
-    public string? vendor_id { get; set; }
     public uint? mothership_mmsi { get; set; }
     public ushort? utc_year { get; set; }
     public byte? utc_month { get; set; }
@@ -48,13 +34,10 @@ public class DTOWebObjectData
     public byte? utc_second { get; set; }
     public byte? aid_type { get; set; }
     public bool? off_position { get; set; }
-    public bool? virtual_aid_flag { get; set; }
-    public bool? gnss_position_status { get; set; }
     public float? magnetic_declination { get; set; }
     public string? source_id { get; set; }
     public int? station_id { get; set; }
     public string? station_name { get; set; }
-    public string? station_operator_name { get; set; }
 
     public DateTime updated { get; set; }
     public WebObjectType? object_type { get; set; }
