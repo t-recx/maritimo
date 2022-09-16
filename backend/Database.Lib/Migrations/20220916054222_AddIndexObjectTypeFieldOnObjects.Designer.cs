38a2df0228440545abe0a4e33ff5aea7e5f2c9c3
João Bruno <hi@joaobruno.xyz>
2022-09-16 06:43:23 +0100

    Added migration for object_type index

--- backend/Database.Lib/Migrations/20220916054222_AddIndexObjectTypeFieldOnObjects.Designer.cs
+++ backend/Database.Lib/Migrations/20220916054222_AddIndexObjectTypeFieldOnObjects.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Database.Lib;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Database.Lib.Migrations
 {
     [DbContext(typeof(MaritimoContext))]
-    partial class MaritimoContextModelSnapshot : ModelSnapshot
+    [Migration("20220916054222_AddIndexObjectTypeFieldOnObjects")]
+    partial class AddIndexObjectTypeFieldOnObjects
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/Database.Lib/Migrations/20220916054222_AddIndexObjectTypeFieldOnObjects.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Database.Lib.Migrations
+{
+    public partial class AddIndexObjectTypeFieldOnObjects : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Objects_object_type",
+                table: "Objects",
+                column: "object_type");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Objects_object_type",
+                table: "Objects");
+        }
+    }
+}
